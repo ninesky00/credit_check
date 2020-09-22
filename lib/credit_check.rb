3ca55fe32232438206a34154c1cae438edc6b8d6
ninesky00 <jiangzho@gmail.com>
2020-09-21 22:31:51 -0500

    add credit_check.rb

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,38 @@
-card_number = "5541808923795240"
-
 # Your Luhn Algorithm Here
+def luhn_algorithm (credit_card)
+    numbers = credit_card.chars.map(&:to_i)
+    double = []
+    numbers.each_with_index do |num, idx|
+        if idx.even?
+            if num*2 > 9
+                double << num*2 -9
+            else 
+                double << num*2
+            end
+        else 
+            double <<  num
+        end     
+    end
+    total = double.reduce {|sum, num| sum += num}
+    valid = total % 10 == 0
+    return valid
+end
 
+class CreditCard
+    attr_reader :card_number, :limit
+    def initialize(card_number, limit=15000)
+        @card_number = card_number
+        @limit = limit
+    end
+
+    def is_valid?
+        p luhn_algorithm(card_number)
+    end
+
+    def last_four
+        "#{card_number[-4..-1]}"
+    end
+end
 # Output
 ## If it is valid, print "The number [card number] is valid!"
 ## If it is invalid, print "The number [card number] is invalid!"
--- test/bank_test.rb
+++ test/bank_test.rb
@@ -1,7 +1,9 @@
+gem 'minitest', '~> 5.2'
+require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/bank'
-require './lib/credit_card'
+require_relative '../lib/bank'
+require_relative '../lib/credit_check'
 
 class BankTest < Minitest::Test
   def test_it_exists
@@ -16,7 +18,7 @@ class BankTest < Minitest::Test
 
   def test_it_knows_a_transaction_is_invalid_when_credit_card_is_invalid
     bank = Bank.new("Wells Fargo")
-    credit_card = CreditCard.new("4024007106512380", 15000)
+    credit_card = CreditCard.new("342801633855673", 15000)
     assert_equal false, bank.valid_transaction?(100, credit_card)
   end
 
@@ -31,4 +33,29 @@ class BankTest < Minitest::Test
     credit_card = CreditCard.new("5541808923795240", 15000)
     assert_equal true, bank.valid_transaction?(11000, credit_card)
   end
+
+  def test_it_knows_card_number
+    bank = Bank.new("Chase")
+    credit_card = CreditCard.new("342804633855673")
+    assert_equal "342804633855673", credit_card.card_number
+  end
+
+  def test_if_card_number_is_valid
+    bank = Bank.new("Chase")
+    credit_card = CreditCard.new("5541808923795240")
+    assert credit_card.is_valid?
+  end
+
+  def test_if_card_number_is_invalid
+    bank = Bank.new("Chase")
+    credit_card = CreditCard.new("4024007106512380")
+    refute credit_card.is_valid?
+  end
+
+  def test_it_can_give_last_four
+    bank = Bank.new("Chase")
+    credit_card = CreditCard.new("342804633855673")
+    assert_equal "5673", credit_card.last_four
+  end
 end
+
